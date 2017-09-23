a8f33f61f63a13d311c09644b438bea16334685e
Ryan <ryanbusbyalt@gmail.com>
2017-09-23 15:00:17 -0400

    Commented code

--- zerg.py
+++ zerg.py
@@ -13,7 +13,7 @@ from random import randint, choice
 class Zerg:
     """ This is the basic parent class that all Zerg come from.  This should
     have been where a lot of the default information was stored about every
-    zerg.
+    Zerg.
     """
     pass
 
@@ -30,15 +30,22 @@ class Drone(Zerg):
         self.home = tuple()
     
     def steps(self):
+        """Returns the total steps used from the Zergs."""
         return self.step
     
     def action(self, context):
-        # Push this off to a map class or something
+        """
+        This runs through and updates a personal map for each Zerg.
+        This provides simple logic to try and keep them alive as they explore
+        the map and mine minerals.
+        """
+        # This needs to be pushed out to the Overlord
         self.location[tuple((context.x, context.y-1))] = vars(context)['south']
         self.location[tuple((context.x, context.y+1))] = vars(context)['north']
         self.location[tuple((context.x+1, context.y))] = vars(context)['east']
         self.location[tuple((context.x-1, context.y))] = vars(context)['west']
         self.xy = (tuple((context.x, context.y)))
+        #I got the following from following Dave's example
         map_keys = list(self.location)
         minx = min(map_keys)[0]
         maxx = max(map_keys)[0]
@@ -84,26 +91,26 @@ class Drone(Zerg):
                     return check
             new = randint(0, 3)
             if new == 0:
-                if self.location[tuple((context.x, context.y + 1))] != '#':
-                    if self.location[tuple((context.x, context.y + 1))] != '~':
+                if self.location[tuple((context.x, context.y + 1))] != '#'\
+                and self.location[tuple((context.x, context.y + 1))] != '~':
                         return 'NORTH'
                 else:
                     new = randint(0,3)
             elif new == 1:
-                if self.location[tuple((context.x, context.y - 1))] != '#':
-                    if self.location[tuple((context.x, context.y - 1))] != '~':
+                if self.location[tuple((context.x, context.y - 1))] != '#'\
+                and self.location[tuple((context.x, context.y - 1))] != '~':
                         return 'SOUTH'
                 else:
                     new = randint(0,3)
             elif new == 2:
-                if self.location[tuple((context.x + 1, context.y))] != '#':
-                    if self.location[tuple((context.x + 1, context.y))] != '~':
+                if self.location[tuple((context.x + 1, context.y))] != '#'\
+                and self.location[tuple((context.x + 1, context.y))] != '~':
                         return 'EAST'
                 else:
                     new = randint(0,3)
             elif new == 3:
-                if self.location[tuple((context.x - 1, context.y))] != '#':
-                    if self.location[tuple((context.x - 1, context.y))] != '~':
+                if self.location[tuple((context.x - 1, context.y))] != '#'\
+                and self.location[tuple((context.x - 1, context.y))] != '~':
                         return 'WEST'
                 else:
                     new = randint(0,3)
@@ -111,13 +118,18 @@ class Drone(Zerg):
                 return check
 
     def get_init_cost(self):
+        """ This should determined how many minerals it takes to initialize a
+        drone.
+        """
         pass
-        # 10 health = 1 mineral
-        # 5 capacity = 1 mineral
-        # 1 move = 3 minerals
 
 class Miner(Drone):
+    """Miner subclass of drone.
+    """
     def __init__(self):
+        """These stats should have been registered from the Drone or Zerg
+        classes.
+        """
         self.health = 10
         self.moves = 1
         self.capacity = 25
@@ -130,11 +142,20 @@ class Miner(Drone):
 
 
 class Scout(Drone):
+    """Scout subclass of drone.
+    This drone was deemed not worthy due to movement costs.
+    """
     pass
 
 
 class Overlord(Zerg):
+    """The Overlord class is used to control the drones and visualize the map
+    in order to determine whether the map is worth mining.
+    """
+    
     def __init__(self, ticks, refined_minerals):
+        """ This holds all of the needed information for a given Overlord.
+        """
         self.maps = {}
         self.zerg = {}
         self.deployedzerg = {}
@@ -150,9 +171,15 @@ class Overlord(Zerg):
             self.refined_minerals -= 9
 
     def add_map(self, map_id, summary):
+        """This adds a map to the map dictionary list, to include a summary
+        to indicate the density of minerals in the area.
+        """
         self.maps[map_id] = summary
     
     def action(self):
+        """The actions the Overlord takes. RETURN and DEPLOY are randomized
+        at this time.
+        """
         if self.ticks < 10:
             return 'RETURN {}'.format(choice(list(self.zerg.keys())))
         act = randint(0, 3)
@@ -177,4 +204,7 @@ class Dashboard():
         pass
 
     def update():
+        """ This should have updated the Overlord of the map location for the
+        Zerg.
+        """
         return "No Dashboard for you!"
